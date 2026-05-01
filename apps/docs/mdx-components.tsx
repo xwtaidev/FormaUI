16dfecb0e783d6cd91aa8dfd6f511aea7daef1a6
xwtaidev@gmail.com <xwtaidev@gmail.com>
2026-05-01 15:55:31 +0800

    feat(docs): install Fumadocs codeblock renderer

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,5 +1,20 @@
-export function useMDXComponents(components: Record<string, unknown>) {
+import defaultComponents from "fumadocs-ui/mdx";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import type { MDXComponents } from "mdx/types";
+
+export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
+    ...defaultComponents,
+    // HTML `ref` attribute conflicts with `forwardRef`
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components
   };
+}
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return getMDXComponents(components);
 }
